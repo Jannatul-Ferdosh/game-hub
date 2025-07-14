564f14720265ec218601344bdfb48d28cb9ccd94
Jannatul-Ferdosh <106528305+Jannatul-Ferdosh@users.noreply.github.com>
2025-07-14 11:25:46 +0600

    Build search Input

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -20,7 +20,6 @@ const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
           <MenuItem
             onClick={() => {
               onSelectPlatform(platform);
-              console.log(platform);
             }}
             key={platform.id}
           >
